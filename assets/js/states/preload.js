3e0e5beab8c297ccaefc3125871fd2796b37c2ea
THAUT627 <theo.rakotomavo@gmail.com>
2025-11-30 13:13:35 +0100

    Ajout des d'autres cartes pour le niveau lava defense.

--- assets/js/states/preload.js
+++ assets/js/states/preload.js
@@ -1,15 +1,15 @@
 var tinydefence = tinydefence || {};
 
-tinydefence.preload = function(game) {};
+tinydefence.preload = function (game) { };
 
 tinydefence.preload.prototype = {
 
-	preload: function() {
+	preload: function () {
 		this.game.load.image('logo', 'assets/images/logo.png');
 		this.game.load.spritesheet('selection', 'assets/images/selection.png', 16, 16);
 		this.game.load.spritesheet('enemy', 'assets/images/enemy.png', 16, 16);
 		this.game.load.spritesheet('crab', 'assets/images/enemyCrab.png', 16, 16);
-		
+
 		this.game.load.image('buildmenu', 'assets/images/ui/menuElements.png');
 		this.game.load.spritesheet('buildmenuButtons', 'assets/images/ui/menuButtons.png', 16, 16);
 
@@ -17,15 +17,33 @@ tinydefence.preload.prototype = {
 		this.game.load.spritesheet('buttonLevel', 'assets/images/ui/buttonLevel.png', 225, 35);
 		this.game.load.spritesheet('buttonMenuNav', 'assets/images/ui/buttonMenuNav.png', 20, 18);
 
-		this.game.load.bitmapFont('font_white', 
+		this.game.load.bitmapFont('font_white',
 			'assets/fonts/font.png',
 			'assets/fonts/font.fnt');
-		this.game.load.bitmapFont('font_green', 
+		this.game.load.bitmapFont('font_green',
 			'assets/fonts/font_green.png',
 			'assets/fonts/font_green.fnt');
-		this.game.load.bitmapFont('font_red', 
+		this.game.load.bitmapFont('font_red',
 			'assets/fonts/font_red.png',
 			'assets/fonts/font_red.fnt');
+
+		const lavaMapOptions = [
+			{ json: 'map1.json', png: 'map1.png', start: { x: 0, y: 3 }, end: { x: 25, y: 1 } },
+			{ json: 'map2.json', png: 'map2.png', start: { x: 3, y: 0 }, end: { x: 27, y: 14 } },
+			{ json: 'map3.json', png: 'map3.png', start: { x: 6, y: 12 }, end: { x: 29, y: 6 } },
+		];
+
+		const randomLava = Phaser.ArrayUtils.getRandomItem(lavaMapOptions);
+
+		const lavaMap = tinydefence.maps.find(m => m.key === 'LavaDefense');
+
+		lavaMap.data = 'assets/maps/LavaDefense/' + randomLava.json;
+		lavaMap.sprite = 'assets/maps/LavaDefense/' + randomLava.png;
+
+		lavaMap.start = randomLava.start;
+		lavaMap.end = randomLava.end;
+
+
 
 		// Load all defined maps in maps.js
 		tinydefence.maps.forEach(map => {
@@ -34,13 +52,13 @@ tinydefence.preload.prototype = {
 		});
 
 		// Load background music
-		this.game.load.audio('bgm', 'assets/audio/bgm.mp3'); 
+		this.game.load.audio('bgm', 'assets/audio/bgm.mp3');
 
 		// Load all tower assets
 		tinydefence.towerManager.load();
 	},
-	
-	create: function() {
+
+	create: function () {
 		this.game.state.start("Menu");
 	}
 }
